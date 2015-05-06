7e5b07c475412e70d5ab27226891c0c7361184d7
Maxim Egorov <megorov@stanford.edu>
2015-05-06 10:32:21 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # DiscreteMDPs
+
+This package provides a basic interface for working with discrete Markov decision processes (MDPs).
+
+## Basic Types
+
+The basic types are
+
+- `DiscreteMDP`
+- `Solver`
+- `Simulator`
+- `Policy`
+- `MDPFile`
+- `PolicyFile`
+
+## Basic functions
+
+- `solve(solver::Solver, mdp::DiscreteMDP)`
+- `simulate(simulator::Simulator, mdp::DiscreteMDP, policy::Policy)`
+
+## File IO
+
+- `load(file::PolicyFile)`
+- `load(file::MDPFile)`
+- `save(file::PolicyFile, policy::Policy)`
+- `save(file::MDPFile, mdp::DiscreteMDP)
+
+## Model functions
+
+- `states(mdp::DiscreteMDP)` returns something that might be iterable (e.g., 1:100)
+- `actions(mdp::DiscreteMDP)` returns something that might be iterable (e.g., 1:100)
+- `actions(mdp::DiscreteMDP, state)` returns something that might be iterable (e.g., 1:100)
+- `reward(mdp::DiscreteMDP, state, action)` returns reward
+- `transition(mdp::DiscreteMDP, state, action, nextState)` returns probability
+- `nextStates(mdp::DiscreteMDP, state, action)` returns arrays of state indices and their corresponding probabilities 
+
--- src/DiscreteMDPs.jl
+++ src/DiscreteMDPs.jl
@@ -1,5 +1,41 @@
+# defines API for discrete MDPs and their solvers
 module DiscreteMDPs
 
-# package code goes here
+# basic types
+export DiscreteMDP, Solver, Simulator, Policy, MDPFile, PolicyFile
+
+abstract DiscreteMDP
+abstract Solver
+abstract Simulator
+abstract Policy
+abstract MDPFile
+abstract PolicyFile
+
+# module function
+export solve, simulate
+
+solve(solver::Solver, mdp::DiscreteMDP) = error("$(typeof(solver)) does not implement solve for MDP $(typeof(mdp))")
+simulate(simulator::Simulator, mdp::DiscreteMDP, policy::Policy) = error("$(typeof(simulator)) does not implement simulate for MDP $(typeof(mdp)) and policy $(typeof(policy))")
+
+# file io
+export load, save
+
+load(file::PolicyFile) = error("$(typeof(file)) does not implement load")
+load(file::MDPFile) = error("$(typeof(file)) does not implement load")
+save(file::PolicyFile, policy::Policy) = error("$(typeof(file)) does not implement save for policy $(typeof(policy))")
+save(file::MDPFile, mdp::DiscreteMDP) = error("$(typeof(file)) does not implement save for DiscreteMDP $(typeof(mdp))")
+
+# model function
+export states, actions, reward, transition, nextStates, numStates, numActions
+
+states(mdp::DiscreteMDP) = error("$(typeof(mdp)) does not implement states") # returns something that might be iterable (e.g., 1:100)
+actions(mdp::DiscreteMDP) = error("$(typeof(mdp)) does not implement actions") # returns something that might be iterable (e.g., 1:100)
+reward(mdp::DiscreteMDP, state, action) = error("$(typeof(mdp)) does not implement reward") # returns reward
+transition(mdp::DiscreteMDP, state, action, nextState) = error("$(typeof(mdp)) does not implement transition") # returns probability
+nextStates(mdp::DiscreteMDP, state, aciton) = error("$(typeof(mdp)) does not implement transition") # returns neighbor states and prob vectors
+
+numStates(mdp::DiscreteMDP) = length(collect(states(mdp)))
+numActions(mdp::DiscreteMDP) = length(collect(actions(mdp)))
 
 end # module
+
